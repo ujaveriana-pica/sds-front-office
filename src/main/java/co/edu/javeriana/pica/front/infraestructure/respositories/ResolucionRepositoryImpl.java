7cb036015bd56b0a84d79dff3b944765daaa9458
jccastellanos <jccastellanos@grupoasd.com.co>
2021-11-26 17:18:01 -0500

    Correccion nombre resolucion

--- src/main/java/co/edu/javeriana/pica/front/infraestructure/respositories/ResolucionRepositoryImpl.java
+++ src/main/java/co/edu/javeriana/pica/front/infraestructure/respositories/ResolucionRepositoryImpl.java
@@ -5,6 +5,7 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.io.File;
+import java.util.Locale;
 import java.util.Optional;
 
 @ApplicationScoped
@@ -15,7 +16,7 @@ public class ResolucionRepositoryImpl implements ResolucionRepository {
 
     @Override
     public Optional<File> download(String id) {
-        String filename = String.format("%s.pdf", id);
+        String filename = String.format("Resolucion_%s.pdf", id.toUpperCase());
         File fileDownload = new File(data + File.separator + filename);
         if (fileDownload.exists()) {
             return Optional.of(fileDownload);
