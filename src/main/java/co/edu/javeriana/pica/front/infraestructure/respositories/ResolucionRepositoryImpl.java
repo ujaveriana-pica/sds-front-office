886649538f2cb7d9117a93e29d99b8a06d2d21d6
Juan Carlos Castellanos M <juancastellanosm@gmail.com>
2021-11-26 17:23:11 -0500

    Feature/resolucion download (#15)
    * Corrección nombre resolucion

--- src/main/java/co/edu/javeriana/pica/front/infraestructure/respositories/ResolucionRepositoryImpl.java
+++ src/main/java/co/edu/javeriana/pica/front/infraestructure/respositories/ResolucionRepositoryImpl.java
@@ -2,7 +2,6 @@ package co.edu.javeriana.pica.front.infraestructure.respositories;
 
 import co.edu.javeriana.pica.front.core.interfaces.ResolucionRepository;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import javax.enterprise.context.ApplicationScoped;
 import java.io.File;
 import java.util.Optional;
@@ -15,7 +14,7 @@ public class ResolucionRepositoryImpl implements ResolucionRepository {
 
     @Override
     public Optional<File> download(String id) {
-        String filename = String.format("%s.pdf", id);
+        String filename = String.format("Resolucion_%s.pdf", id.toUpperCase());
         File fileDownload = new File(data + File.separator + filename);
         if (fileDownload.exists()) {
             return Optional.of(fileDownload);
